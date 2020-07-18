e756243e1b194c53953e8eca6a720b5501dc4556
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-07-17 21:28:59 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -69,7 +69,7 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
        SELECT * FROM students WHERE grade = 10 LIMIT ?
     SQL
